0b9d6764baf85f483ee8a8306d1722e04062f83c
gagoory7 <gagoory7@gmail.com>
2024-10-22 20:51:58 +0900

    feat: Improve real-time status and enhance readability

--- src/preprocessing/remove_duplicates.py
+++ src/preprocessing/remove_duplicates.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import time
+from tqdm import tqdm
 
 
 ct = time.time()
@@ -9,19 +10,10 @@ ct = time.time()
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 
+train_data = pd.read_csv('data/train.csv',index_col=0)
 
 
-home = '/data/ephemeral/home/project_bsm'
-
-
-
-train_data = pd.read_csv(home+'/data/train.csv',index_col=0)
-
-
-
-location = ['latitude','longitude','floor','area_m2','built_year']
-
-
+# 데이터 전처리
 
 train_data['year_month'] = train_data['contract_year_month'].astype(str)
 train_data['year'] = train_data['year_month'].str[:4].astype(int) 
@@ -39,24 +31,19 @@ train_data['time_day'] = (train_data['date'] - start_date).dt.days
 
 df = train_data.copy()
 
-all_loc = df.drop_duplicates(subset=location).reset_index(drop=True)
-
 
 print('Start')
 
-i = 0
-ct = time.time()
-count = 1
+tqdm.pandas()
+
+location = ['latitude','longitude','floor','area_m2','built_year']
+all_loc = df.drop_duplicates(subset=location).reset_index(drop=True)
+
+
+# 입금액이 같은 경우 중복 제거해서 추출
 def search_df_fun(x):
-    global i
     global df
-    global search_df
     global search_loc_df
-    
-    if i % 1000 == 0 :
-        print('\n\n\n' + str(i) + '\n\n\n')
-        print('time :', time.time()-ct)
-    i+=1 
     
     x_loc = x[location]
 
@@ -66,74 +53,62 @@ def search_df_fun(x):
         (df['floor'] == x_loc['floor']) &
         (df['area_m2'] == x_loc['area_m2']) &
         (df['built_year'] == x_loc['built_year'])
-    ].drop_duplicates(subset=location+['deposit'])
+    ].drop_duplicates(subset=location + ['deposit'])
     
+    search_loc_df.apply(search_same_df, axis=1)
 
-    search_loc_df.apply(search_same_df,axis=1)
 
-
+# 입금액이 같은 모든 경우에 대해 중복 검사 
 def search_same_df(x):
     global search_df
     global df
-    global count
-    
+ 
 
-    x_loc =x[location+['deposit']]
-    
+    x_loc = x[location + ['deposit']]
 
     search_df = df[
         (df['latitude'] == x_loc['latitude']) &
         (df['longitude'] == x_loc['longitude']) &
         (df['floor'] == x_loc['floor']) &
         (df['area_m2'] == x_loc['area_m2']) &
-        (df['built_year'] == x_loc['built_year'])&
+        (df['built_year'] == x_loc['built_year']) &
         (df['deposit'] == x_loc['deposit'])
     ]
 
-    while not search_df.empty:  # df가 비어있지 않으면 True
-        search_df_del_fun(search_df.iloc[0,:])
+    while not search_df.empty:
+        search_df_del_fun(search_df.iloc[0, :])
 
-        if count % 10000 ==0 :
-            print('\n\n\n\n', count , '\n\n\n\n')
+
 
 def search_df_del_fun(x):
     global search_df
     global df
-    global count
+
 
     x_time = x['time_day']
     
     mask_prev = (search_df['time_day'] <= x_time + 28) & (x_time <= search_df['time_day'])
     
-    print('현재 index :', x.name, '몇개 같은지 :', np.sum(mask_prev), end = ' ')
-    
     drop_index = list(search_df[mask_prev].index)
-
-    count += len(drop_index)
     
-    if drop_index is None :
-        print('\n\n\n\n\n 뭔가 잘못됨 \n\n\n\n\n',drop_index)
+    if drop_index is None:
+        print('\n\n\n\n\n ERROR \n\n\n\n\n', drop_index)
         return
     
-    elif len(drop_index) == 1 :
-        search_df.drop(drop_index,inplace=True)
-        print('drop은 해당 열만 :' ,drop_index)
-    else :
-        print('drop 열 들 ',drop_index)
-        search_df.drop(drop_index,inplace=True)
-        if x.name in drop_index :
-          drop_index.remove(x.name)
-        df.drop(drop_index,inplace=True)
-
+    elif len(drop_index) == 1:
+        search_df.drop(drop_index, inplace=True)
         
-
-all_loc.apply(search_df_fun,axis=1)
-
-print(time.time()-ct)
-
-
-# In[ ]:
+    else:
+        search_df.drop(drop_index, inplace=True)
+        if x.name in drop_index:
+            drop_index.remove(x.name)
+        df.drop(drop_index, inplace=True)
 
 
-df.to_csv(home+'/효율개선.csv')
+# tqdm을 사용한 progress bar 추가
+all_loc.progress_apply(search_df_fun, axis=1)
+
+print('End')
+
+df.to_csv('../drop_duplicate_train_df.csv')
 
