b7dbdda5b1b1d2d969abf27737350aa16e376028
Seongyun <seongyungim99@gmail.com>
2024-10-28 11:45:19 +0900

    Mod : Add deposit_per_area & final output

--- src/models/lgbm_cluster.py
+++ src/models/lgbm_cluster.py
@@ -12,32 +12,33 @@ np.random.seed(RANDOM_SEED)
 
 train_df = pd.read_csv("../../data/train_aftercountplace.csv")
 test_df = pd.read_csv("../../data/test_aftercountplace.csv")
-
-train_df = train_df.drop(columns=['index'])
-test_df = test_df.drop(columns=['index'])
+sample_submission = pd.read_csv("../../data/sample_submission.csv")
 
 holdout_start = 202307
 holdout_end = 202312
 valid_df = train_df[(train_df['contract_year_month'] >= holdout_start) & (train_df['contract_year_month'] <= holdout_end)]
 final_train_df = train_df[~((train_df['contract_year_month'] >= holdout_start) & (train_df['contract_year_month'] <= holdout_end))]
 
-X_train = final_train_df.drop(columns=['deposit'])
-y_train = final_train_df['deposit']
-X_valid = valid_df.drop(columns=['deposit'])
+train_df = train_df.drop(columns=['index', 'contract_day'])
+test_df = test_df.drop(columns=['index', 'contract_day'])
+
+train_df['deposit_per_area'] = train_df['deposit'] / train_df['area_m2']
+
+X_train = final_train_df.drop(columns=['deposit_per_area', 'deposit', 'contract_year_month'])
+y_train = final_train_df['deposit_per_area']
+X_valid = valid_df.drop(columns=['deposit_per_area', 'deposit', 'contract_year_month'])
 y_valid = valid_df['deposit']
 X_test = test_df.copy()
+X_test = X_test.drop(columns=['contract_year_month'])
 
-X_total = train_df.drop(columns=['deposit'])
-y_total = train_df['deposit']
+X_total = train_df.drop(columns=['deposit_per_area', 'deposit', 'contract_year_month'])
+y_total = train_df['deposit_per_area']
 
 # train + valid 데이터로 최적의 k 찾기
 best_k = 10
 kmeans = KMeans(n_clusters=best_k, random_state=RANDOM_SEED)
 kmeans.fit(X_total[['latitude', 'longitude']])
 total_pred = kmeans.predict(X_total[['latitude', 'longitude']])
-
-# test 데이터에 대한 cluster 예측
-test_pred = kmeans.predict(X_test[['latitude', 'longitude']])
 
 lgb_models = []
 best_iterations = []
@@ -83,3 +84,36 @@ valid_pred = X_valid['pred'] * X_valid['area_m2']
 valid_mae = mean_absolute_error(y_valid, valid_pred)
 
 print(valid_mae)
+
+# valid set을 포함한 최종 학습
+lgb_models = []
+total_pred = kmeans.predict(X_total[['latitude', 'longitude']])
+X_total = X_total.drop(columns=['latitude', 'longitude'])
+for i in range(best_k):
+    print(f'Cluster {i} modeling...')
+    total_cluster_idx = np.where(total_pred == i)[0]   # (index_array, dtype)
+
+    X_total_cluster = X_total.iloc[total_cluster_idx]
+    y_total_cluster = y_total.iloc[total_cluster_idx]
+
+    lgbm_params = lgb_models_params[i]
+
+    lgb_model = lgb.LGBMRegressor(**lgbm_params)
+    lgb_model.fit(X_total_cluster, y_total_cluster, eval_metric='l2')
+
+    lgb_models.append(lgb_model)
+
+# test 데이터에 대한 cluster 예측
+test_pred = kmeans.predict(X_test[['latitude', 'longitude']])
+
+X_test['pred'] = 0
+X_test = X_test.drop(columns=['latitude', 'longitude'])
+for i in range(best_k):
+    test_cluster_idx = np.where(test_pred == i)[0]
+    X_test_cluster = X_test.iloc[test_cluster_idx]
+    X_test.loc[X_test_cluster.index, 'pred'] = lgb_models[i].predict(X_test_cluster.drop(columns=['pred']))
+
+test_pred_xgb_cluster = X_test['pred'] * X_test['area_m2']
+
+sample_submission['deposit'] = test_pred_xgb_cluster
+sample_submission.to_csv('../../results/output.csv', index=False, encoding='utf-8-sig')
