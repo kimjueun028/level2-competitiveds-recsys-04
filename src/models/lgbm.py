b55443dfab783623019b29c2caf1941f6a7e080a
gagoory7 <gagoory7@gmail.com>
2024-10-28 14:28:40 +0900

    refactor: rename code, remove unused code, and add comments

--- src/models/lgbm.py
+++ src/models/lgbm.py
@@ -3,49 +3,30 @@ import pandas as pd
 import numpy as np
 import lightgbm as lgb
 import yaml
-
 from sklearn.metrics import mean_absolute_error
 from sklearn.cluster import KMeans
-
 import sys
-sys.path.append('../')
+sys.path.append('.')
 from src.utils.age_group import age_grouping
 
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 
+
 # Data load
-train_df = pd.read_csv("../../data/train_aftercountplace.csv")
-test_df = pd.read_csv("../../data/test_aftercountplace.csv")
-sample_submission = pd.read_csv("../../data/sample_submission.csv")
+train_df = pd.read_csv("data/train_lgbm.csv")
+test_df = pd.read_csv("data/test_lgbm.csv")
+sample_submission = pd.read_csv("data/sample_submission.csv")
 
-# age feature grouping
-train_df = age_grouping(train_df)
-test_df = age_grouping(test_df)
-
-# 필요없는 feature 삭제
 train_df = train_df.drop(columns=['index', 'contract_day', 'age'])
 test_df = test_df.drop(columns=['index', 'contract_day', 'age'])
 
-# deposit_per_area 계산
-train_df['deposit_per_area'] = train_df['deposit'] / train_df['area_m2']
-
-# validataion을 위한 holdout set 분리
-holdout_start = 202307
-holdout_end = 202312
-valid_df = train_df[(train_df['contract_year_month'] >= holdout_start) & (train_df['contract_year_month'] <= holdout_end)]
-final_train_df = train_df[~((train_df['contract_year_month'] >= holdout_start) & (train_df['contract_year_month'] <= holdout_end))]
-
 # X, y 분리
-X_train = final_train_df.drop(columns=['deposit_per_area', 'deposit', 'contract_year_month'])
-y_train = final_train_df['deposit_per_area']
-X_valid = valid_df.drop(columns=['deposit_per_area', 'deposit', 'contract_year_month'])
-y_valid = valid_df['deposit']
-X_test = test_df.copy()
-X_test = X_test.drop(columns=['contract_year_month'])
-
+train_df['deposit_per_area'] = train_df['deposit'] / train_df['area_m2']
 X_total = train_df.drop(columns=['deposit_per_area', 'deposit', 'contract_year_month'])
 y_total = train_df['deposit_per_area']
+X_test = test_df.copy()
+X_test = X_test.drop(columns=['contract_year_month'])
 
 # k = 10으로 KMeans fit
 best_k = 10
@@ -55,53 +36,13 @@ kmeans.fit(X_total[['latitude', 'longitude']])
 # train, valid set에 KMeans 적용
 lgb_models = []
 best_iterations = []
-train_pred = kmeans.predict(X_train[['latitude', 'longitude']])
-valid_pred = kmeans.predict(X_valid[['latitude', 'longitude']])
-X_train = X_train.drop(columns=['latitude', 'longitude'])
-X_valid = X_valid.drop(columns=['latitude', 'longitude'])
 
 # lgbm 하이퍼파라미터 load
-def load_lgbm_models_params(yaml_file_path):
-    with open(yaml_file_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config['lgb_models_params']
+with open('config/lgbm_params.yaml','r') as f :
+    config = yaml.safe_load(f)
+    lgb_models_params = config['lgb_models_params']
 
-yaml_file_path = '../../config/lgbm_params.yaml'
-lgb_models_params = load_lgbm_models_params(yaml_file_path)
-
-# train 학습
-for i in range(best_k):
-    print(f'Cluster {i} modeling...')
-    train_cluster_idx = np.where(train_pred == i)[0]   # (index_array, dtype)
-    valid_cluster_idx = np.where(valid_pred == i)[0]
-
-    X_train_cluster = X_train.iloc[train_cluster_idx]
-    y_train_cluster = y_train.iloc[train_cluster_idx]
-
-    X_valid_cluster = X_valid.iloc[valid_cluster_idx]
-    y_valid_cluster = y_valid.iloc[valid_cluster_idx]
-
-    lgbm_params = lgb_models_params[i]
-
-    lgb_model = lgb.LGBMRegressor(**lgbm_params)
-    lgb_model.fit(X_train_cluster, y_train_cluster, eval_set=[(X_valid_cluster, y_valid_cluster)], eval_metric='l2')
-    best_iterations.append(lgb_model.best_iteration_)
-
-    lgb_models.append(lgb_model)
-
-# valid 예측
-X_valid['pred'] = 0
-for i in range(best_k):
-    valid_cluster_idx = np.where(valid_pred == i)[0]
-    X_valid_cluster = X_valid.iloc[valid_cluster_idx]
-    X_valid.loc[X_valid_cluster.index, 'pred'] = lgb_models[i].predict(X_valid_cluster.drop(columns=['pred']))
-
-valid_pred = X_valid['pred'] * X_valid['area_m2']
-valid_mae = mean_absolute_error(y_valid, valid_pred)
-
-print(valid_mae)
-
-# valid set을 포함한 최종 학습
+# 최종 학습
 lgb_models = []
 total_pred = kmeans.predict(X_total[['latitude', 'longitude']])
 X_total = X_total.drop(columns=['latitude', 'longitude'])
